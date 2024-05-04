30f62494ae8a88c8df8ccbc871238f621cdf7731
Bohdan Martyniuk <bohdan@growthcurve.co>
2024-05-04 16:44:06 -0400

    feat: converted personal info to form

--- src/app/dashboard/profile-settings/components/personalInfo/personalInfo.tsx
+++ src/app/dashboard/profile-settings/components/personalInfo/personalInfo.tsx
@@ -8,7 +8,7 @@ import {
   setLastName,
 } from "@/src/app/lib/store/features/userProfile/userProfile";
 import addUserData from "@/src/firebase/firestore/addUserData";
-import { Alert, Button, Card, TextInput } from "flowbite-react";
+import { Alert, Button, Card, Label, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { HiOutlinePencilAlt } from "react-icons/hi";
 import { useDispatch } from "react-redux";
@@ -50,7 +50,8 @@ export default function PersonalInfo() {
     setNewfirstName(firstName);
   };
 
-  const saveChanges = async () => {
+  const onChangePersonalInfo = async (event: React.FormEvent) => {
+    event.preventDefault();
     if (user?.uid && newValuesValid && personalInfoChanged) {
       const { error, errorMessage } = await addUserData(user.uid, {
         firstName: newFirstName.trim(),
@@ -73,96 +74,100 @@ export default function PersonalInfo() {
   return (
     <div className="mt-5">
       <Card className="bg-gray-100 w-full">
-        <div className="flex justify-between">
-          <h2 className="text-xl font-medium">Personal Info</h2>
-          <div>
-            {cardStatus === CARD_ACTION_STATUS.READ ? (
-              <button
-                onClick={() => {
-                  setCardStatus(CARD_ACTION_STATUS.EDIT);
-                  setApiStatus(API_STATUS.IDLE);
-                }}
-              >
-                <HiOutlinePencilAlt />
-              </button>
+        <form onSubmit={onChangePersonalInfo}>
+          <div className="flex justify-between">
+            <h2 className="text-xl font-medium">Personal Info</h2>
+            <div>
+              {cardStatus === CARD_ACTION_STATUS.READ ? (
+                <button
+                  onClick={() => {
+                    setCardStatus(CARD_ACTION_STATUS.EDIT);
+                    setApiStatus(API_STATUS.IDLE);
+                  }}
+                >
+                  <HiOutlinePencilAlt />
+                </button>
+              ) : null}
+            </div>
+          </div>
+          {apiStatus === API_STATUS.SUCCESS ? (
+            <Alert className="mt-5" color="success" onDismiss={handleCloseSuccessAlert}>
+              <span className="font-medium">Success!</span> Your personal
+              information has been updated updated!
+            </Alert>
+          ) : null}
+          {apiStatus === API_STATUS.ERROR ? (
+            <Alert className="mt-5" color="failure" onDismiss={handleCloseSuccessAlert}>
+              <span className="font-medium">Error!</span>{" "}
+              {apiErrorMessage || "Plese try again later."}
+            </Alert>
+          ) : null}
+
+          {/* first name */}
+          <div className="mt-5 bg-white rounded-lg py-5 px-3">
+            <div className="flex justify-between items-center">
+              <div>
+                <Label htmlFor="first-name">First Name</Label>
+              </div>
+              <div>
+                {cardStatus === CARD_ACTION_STATUS.READ ? (
+                  <p className="font-medium">{firstName}</p>
+                ) : null}
+              </div>
+            </div>
+            {cardStatus === CARD_ACTION_STATUS.EDIT ? (
+              <TextInput
+                id="first-name"
+                className="mt-2"
+                value={newFirstName}
+                onChange={({ target }) => setNewfirstName(target.value)}
+                minLength={NAME_MIN_LENGTH}
+                maxLength={NAME_MAX_LENGTH}
+              />
             ) : null}
           </div>
-        </div>
-        {apiStatus === API_STATUS.SUCCESS ? (
-          <Alert color="success" onDismiss={handleCloseSuccessAlert}>
-            <span className="font-medium">Success!</span> Your personal
-            information have been updated successfully!
-          </Alert>
-        ) : null}
-        {apiStatus === API_STATUS.ERROR ? (
-          <Alert color="failure" onDismiss={handleCloseSuccessAlert}>
-            <span className="font-medium">Error!</span>{" "}
-            {apiErrorMessage || "Plese try again later."}
-          </Alert>
-        ) : null}
 
-        {/* first name */}
-        <div className="mt-5 bg-white rounded-lg py-5 px-3">
-          <div className="flex justify-between items-center">
-            <div>
-              <p>First Name</p>
+          {/* last name */}
+          <div className="bg-white rounded-lg py-5 px-3 mt-5">
+            <div className="flex justify-between items-center">
+              <div>
+                <Label htmlFor="last-name">Last Name</Label>
+              </div>
+              <div>
+                {cardStatus === CARD_ACTION_STATUS.READ ? (
+                  <p className="font-medium">{lastName}</p>
+                ) : null}
+              </div>
             </div>
-            <div>
-              {cardStatus === CARD_ACTION_STATUS.READ ? (
-                <p className="font-medium">{firstName}</p>
-              ) : null}
-            </div>
+            {cardStatus === CARD_ACTION_STATUS.EDIT ? (
+              <TextInput
+                id="last-name"
+                className="mt-2"
+                value={newLastName}
+                onChange={({ target }) => setNewLastName(target.value)}
+                minLength={NAME_MIN_LENGTH}
+                maxLength={NAME_MAX_LENGTH}
+              />
+            ) : null}
           </div>
-          {cardStatus === CARD_ACTION_STATUS.EDIT ? (
-            <TextInput
-              className="mt-2"
-              value={newFirstName}
-              onChange={({ target }) => setNewfirstName(target.value)}
-              minLength={NAME_MIN_LENGTH}
-              maxLength={NAME_MAX_LENGTH}
-            />
+
+          {cardStatus === CARD_ACTION_STATUS.EDIT &&
+          apiStatus !== API_STATUS.SUCCESS ? (
+            <div className="flex space-x-5 mt-5">
+              <Button
+                type="submit"
+                disabled={
+                  !personalInfoChanged || !firstNameValid || !lastNameValid
+                }
+              >
+                Save
+              </Button>
+              <Button color="light" onClick={cancelChanges}>
+                Cancel{" "}
+              </Button>
+            </div>
           ) : null}
-        </div>
-
-        {/* last name */}
-        <div className="bg-white rounded-lg py-5 px-3">
-          <div className="flex justify-between items-center">
-            <div>
-              <p>Last Name</p>
-            </div>
-            <div>
-              {cardStatus === CARD_ACTION_STATUS.READ ? (
-                <p className="font-medium">{lastName}</p>
-              ) : null}
-            </div>
-          </div>
-          {cardStatus === CARD_ACTION_STATUS.EDIT ? (
-            <TextInput
-              className="mt-2"
-              value={newLastName}
-              onChange={({ target }) => setNewLastName(target.value)}
-              minLength={NAME_MIN_LENGTH}
-              maxLength={NAME_MAX_LENGTH}
-            />
-          ) : null}
-        </div>
-
-        {cardStatus === CARD_ACTION_STATUS.EDIT &&
-        apiStatus !== API_STATUS.SUCCESS ? (
-          <div className="flex space-x-5">
-            <Button
-              disabled={
-                !personalInfoChanged || !firstNameValid || !lastNameValid
-              }
-              onClick={saveChanges}
-            >
-              Save
-            </Button>
-            <Button color="light" onClick={cancelChanges}>
-              Cancel{" "}
-            </Button>
-          </div>
-        ) : null}
+        </form>
       </Card>
     </div>
   );
